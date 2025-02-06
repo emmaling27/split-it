2c812b7e60b090bf782dd3664815c789397fca10
Emma Forman Ling <emma@convex.dev>
2025-02-06 15:11:04 -0800

    make resend work

--- convex/auth.ts
+++ convex/auth.ts
@@ -3,5 +3,10 @@ import Resend from "@auth/core/providers/resend";
 import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [GitHub, Resend],
+  providers: [
+    GitHub,
+    Resend({
+      from: "no-reply@updates.listsoflists.app",
+    }),
+  ],
 });
