3eb96f76bc7a6e1220bfa64edab9152289575d01
Emma Forman Ling <emma@convex.dev>
2025-02-22 20:50:48 -0800

    remove github from auth providers

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,10 +1,8 @@
-import GitHub from "@auth/core/providers/github";
 import Resend from "@auth/core/providers/resend";
 import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [
-    GitHub,
     Resend({
       from: "no-reply@updates.listsoflists.app",
     }),
